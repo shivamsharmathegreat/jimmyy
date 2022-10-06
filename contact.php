e48f94f8778a2fd56fe490ae42bd87dc7d8d9ae1
shivamsharmathegreat <115068913+shivamsharmathegreat@users.noreply.github.com>
2022-10-06 14:42:12 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,87 @@
+<?php include 'header.php';?>
+<div class="container-fluid" style= "background-image: linear-gradient(to right, #ff00d4  , #1db5b7);">
+<div class="container p-4">
+<div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12"style="font-size:23px; color:white; text-align:center;"><b>CONTACT WITH US</b><hr style="border:1px solid yellow; width:200px;">
+	</div>
+	</div>
+    
+    <div class="row">	
+	 <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6 pt-2">
+	       <div class="container-fluid" style="height:425px; border:4px solid grey; background-color:white; border-radius:10px;">
+           <div class="row">
+	       <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 p-2" style="background-color:white; color:#ff00d4; text-align:center; font-size:25px;  border-radius:10px; box-shadow: 1px 1px 10px #ff00d4;"><b>CONTACT SUPPORT</b></div>
+		   </div>
+           <div class="row">
+           <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12" style="font-size:18px;"><br><b>Address</b></div>		   
+           </div>
+		   
+           <div class="row">
+           <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12" style="font-size:18px;">B-15 U.P.- 201307</div>		   
+           </div>              
+
+           <div class="row">
+           <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12"  style="font-size:18px;"><br><b>Email Id:</b></div>		   
+           </div>
+
+           <div class="row">
+           <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12" style="font-size:18px;">something@gmail.com</div>		   
+           </div>
+
+           <div class="row">
+           <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12" style="font-size:18px;"><br><b>Phone Number</b></div>		   
+           </div>
+
+           <div class="row">
+           <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12" style="font-size:18px;"> 9711455, 8433127566 </div>		   
+           </div><br>
+
+           </div>
+     </div>
+     
+
+       
+     <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6 pt-2">
+		<div class="container-fluid" style="background-color:#f6f6f6;  height:425px; border:4px solid grey; border-radius:15px;"> 
+	    <div class="row">
+	    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 p-2" style="color:#1db5b7; background-color:white; text-align:center; font-size:25px; border-radius:10px; box-shadow: 1px 1px 10px #1db5b7;"><b>QUICK ENQUIERY</b>
+		</div>
+		</div>
+	    <form method="POST">
+	    <div class="row">
+	    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 "><br><input type="text" placeholder="NAME" name="name" style="width:100%; height:90%;">
+		</div>
+		</div>	 
+	 
+	 	    <div class="row">
+	    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 mt-4"><br><input type="number" name="mob" placeholder="MOBILE NO." style="width:100%; height:90%;">
+		</div>
+		</div>
+	 
+	 	    <div class="row">
+	    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 mt-4"><br><input type="email" name="email" placeholder="EMAIL ID" style="width:100%; height:90%;">
+		</div>
+		</div>
+	 
+	 	    <div class="row">
+	    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 mt-4"><br><input type="text" name="msg" placeholder="Write Your Message" style="width:100%; height:90%;">
+		</div>
+		</div>
+	 
+	 	    <div class="row">
+	    <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12 mt-5" style="text-align:center;"><input type="submit" name="send" style="color:black; background-color:white; border-radius:10px; border:2px solid #1db5b7;">
+		</div>
+		</div>
+	    </div>
+</form>
+    </div>
+    </div>
+	<div class="row mb-4 mt-4">
+		<div class="col-md-12 p-0" style="border:2px solid grey; box-shadow: 1px 1px 10px grey;"> <div class="sons"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3536.1541282121266!2d77.74308871505946!3d27.58875078284119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2402c478a1380cc3!2zMjfCsDM1JzE5LjUiTiA3N8KwNDQnNDMuMCJF!5e0!3m2!1sen!2sin!4v1644073184772!5m2!1sen!2sin" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe> 
+		<a class="aaa" href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3536.1541282121266!2d77.74308871505946!3d27.58875078284119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x2402c478a1380cc3!2zMjfCsDM1JzE5LjUiTiA3N8KwNDQnNDMuMCJF!5e0!3m2!1sen!2sin!4v1644073184772!5m2!1sen!2sin"><div class="tree"><b>GET DIRECTION</b></div></a>
+		</div> 
+	</div>    
+    </div>
+    </div>
+		</div>
+<?php include 'footer.php';?>
